3ea10402437a6d2083149c884b6a64c9a312be20
Manuel Schneider <mschneider@dataforum.de>
2024-12-15 19:24:59 +0100

    Lib

---
+++ AdventOfCodeLibrary/Downloader.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace AdventOfCodeLibrary;
+public static class Downloader
+{
+   public static async Task<string> DownloadInput(int year, int day, string cookie)
+   {
+
+      CookieContainer container = new();
+      container.Add(new Uri("https://adventofcode.com"), new Cookie("session", cookie));
+      var client = new HttpClient(new HttpClientHandler() { CookieContainer = container });
+
+      HttpResponseMessage x = await client.GetAsync($"https://adventofcode.com/{year}/day/{day}/input", CancellationToken.None);
+
+      if (x.IsSuccessStatusCode)
+      {
+        var content = await x.Content.ReadAsStringAsync();
+
+         return content.ReplaceLineEndings();
+      }
+
+      else
+      {
+         return "";
+      }
+   }
+}
---
+++ AdventOfCodeLibrary/Std.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeLibrary
+{
+   public static class Std
+   {
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      public static char[][] ToCharMap(this string input) => input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(x => x.ToCharArray()).ToArray();
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      public static string[] ToLines(this string input) => input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      public static bool SquareAssumption(char[][] map) => map.Length == 0 || (map.All(x => x.Length == map[0].Length) && map.Length == map[0].Length);
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      public static bool IsSquare(this char[][] map) => map.Length == 0 || (map.All(x => x.Length == map[0].Length) && map.Length == map[0].Length);
+
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      public static int GetNumberOfDigits(int number)
+      {
+         int count = 0;
+         do
+         {
+            number /= 10;
+            count++;
+         }
+         while (number != 0);
+         return count;
+      }
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      public static int GetNumberOfDigits(long number)
+      {
+         int count = 0;
+         do
+         {
+            number /= 10;
+            count++;
+         }
+         while (number != 0);
+         return count;
+      }
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      public static int GetNumberOfDigits(string input)
+      {
+         if (!long.TryParse(input, out long result))
+            return -1;
+
+         return GetNumberOfDigits(result);
+      }
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      public static double FastParseDouble(ReadOnlySpan<char> input)
+      {
+         int result = 0;
+         int hasdot = 0;
+         double dot = 0;
+         while (input.Length > 0)
+         {
+            if (input[0] == '.')
+            {
+               hasdot = 1;
+               input = input[1..];
+               continue;
+            }
+            result = (result * 10) + (input[0] - '0');
+            input = input[1..];
+            dot += hasdot;
+         }
+
+         return result / dot;
+      }
+
+   }
+}
